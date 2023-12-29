cc203a5d96d391bed4a1b84a46845e61e83a900c
Satyam Pant <Satyam.pant@gmail.com>
2023-12-29 19:51:31 +0100

    added data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
 
-logger.info("Custom Log")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<< ")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nx=========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ research/01_data_ingestion.py
@@ -0,0 +1,113 @@
+#%%
+import os
+%pwd
+# %%
+os.chdir("../")
+# %%
+%pwd
+# %%
+#entity is return type of any function
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+
+
+# %%
+from src.cnnClassifier.constants import *
+from src.cnnClassifier.utils.common import read_yaml, create_directories
+
+
+# %%
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+
+    
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+
+        create_directories([config.root_dir])
+
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir 
+        )
+
+        return data_ingestion_config
+# %%
+#preparing component
+import os
+import urllib.request as request
+import zipfile
+import gdown
+from src.cnnClassifier import logger
+from src.cnnClassifier.utils.common import get_size
+
+#this class will take data ingestion configuration from class ConfigurationManager there is a clas get_
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    
+    def download_file(self)-> str:
+        '''
+        Fetch data from the url
+        '''
+
+        try: 
+            dataset_url = self.config.source_URL
+            zip_download_dir = self.config.local_data_file
+            os.makedirs("artifacts/data_ingestion", exist_ok=True)
+            logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
+
+            file_id = dataset_url.split("/")[-2]
+            prefix = 'https://drive.google.com/uc?/export=download&id='
+            gdown.download(prefix+file_id,zip_download_dir)
+
+            logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
+
+        except Exception as e:
+            raise e
+        
+    
+
+    def extract_zip_file(self):
+        """
+        zip_file_path: str
+        Extracts the zip file into the data directory
+        Function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
+
+# %%
+#defining the pipeline
+try:
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.extract_zip_file()
+except Exception as e:
+    raise e
+# %%
